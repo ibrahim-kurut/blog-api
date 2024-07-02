08e8eb88129eae99c6010f724da2e793cfc4c5e4
ibrahim kurut <102996120+ibrahim-kurut@users.noreply.github.com>
2024-07-02 09:21:26 +0300

    create post & category models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Post(models.Model):
+
+    STATUS = (
+    (0, 'Draft'),
+    (1, 'Published'),
+    )
+
+
+    title = models.CharField(max_length=200)
+    content = models.TextField(null=True, default="content")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    status = models.PositiveIntegerField(choices=STATUS, default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.status}"
