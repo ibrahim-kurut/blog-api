b1205dfa48454e5c32a8a4edacbe23fff71d09f2
ibrahim kurut <102996120+ibrahim-kurut@users.noreply.github.com>
2024-07-02 13:36:12 +0300

    login

--- main/settings.py
+++ main/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # TPA
     'rest_framework',
+    'rest_framework.authtoken',
 
     # My Apps
     'blog',
@@ -129,3 +130,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
+from rest_framework.authtoken import views
+
 from .views import RegisterCreateAPIView, UserListAPIView
 
 # url register = /blog/api/users/register/
 
 urlpatterns = [
     path('register/', RegisterCreateAPIView.as_view()),
-    path('users/', UserListAPIView.as_view())
+    path('users/', UserListAPIView.as_view()),
+    path('login/', views.obtain_auth_token),
 ]
